3355a68b55d7d9fae7ec067622c1bc52e88d91b6
AnPe92 <93088524+AnPe92@users.noreply.github.com>
2023-04-15 01:08:11 +0200

    asd

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,13 +2,13 @@
 import NextAuth from 'next-auth';
 import GithubProvider from "next-auth/providers/github"
 
-export default NextAuth({
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-        // Add other providers here
-    ],
-    // Add any additional configurations, like a database or callbacks, here
-});
+// export default NextAuth({
+//     providers: [
+//         GithubProvider({
+//             clientId: process.env.GITHUB_ID,
+//             clientSecret: process.env.GITHUB_SECRET,
+//         }),
+//         // Add other providers here
+//     ],
+//     // Add any additional configurations, like a database or callbacks, here
+// });
